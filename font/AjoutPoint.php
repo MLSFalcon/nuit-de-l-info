e3481401155a6cb5e94c51ec6028309022d93417
Nicolas <nicolascharpentier1808@gmail.com>
2024-12-05 21:51:14 +0100

    feature: debut page modification

---
+++ font/AjoutPoint.php
@@ -0,0 +1,8 @@
+<?php
+?>
+<form action=".././gestion/gestionAjoutDePoint.php" method="post">
+    <label><input type="number" required step="any">Position X</label>
+    <label><input type="number" required step="any">Position Y</label>
+    <label><textarea>Texte : </textarea></label>
+    <input type="submit" value="Confirmer ajout">
+</form>
