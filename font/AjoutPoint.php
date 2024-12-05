f7f602a072aec42decd23c03fbee46b1c372a83c
Nicolas <nicolascharpentier1808@gmail.com>
2024-12-05 22:30:21 +0100

    feat: bdd tentative

--- font/AjoutPoint.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-<form action=".././gestion/gestionAjoutDePoint.php" method="post">
-    <label><input type="number" required step="any">Position X</label>
-    <label><input type="number" required step="any">Position Y</label>
-    <label><textarea>Texte : </textarea></label>
-    <input type="submit" value="Confirmer ajout">
-</form>
