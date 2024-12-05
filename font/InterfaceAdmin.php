192bdb2976e6dd02cc5bffabafefc97870815ee8
Nicolas <nicolascharpentier1808@gmail.com>
2024-12-05 22:53:26 +0100

    feat : liste info

--- font/InterfaceAdmin.php
+++ font/InterfaceAdmin.php
@@ -7,16 +7,16 @@
 if (isset($_POST['ajouter'])){
     ?>
     <form action=".././gestion/gestionAjoutDePoint.php" method="post">
-        <label><input type="number" required step="any">Position X</label>
-        <label><input type="number" required step="any">Position Y</label>
-        <label><textarea>Texte : </textarea></label>
+        <label>Position X<input type="number" name="posX" required step="any"></label>
+        <label>Position Y<input type="number" name="posY" required step="any"></label>
+        <label><textarea name="resume">Texte : </textarea></label>
         <input type="submit" name="confirmer" value="ConfirmerAjout">
     </form>
     <?php
 }
 
 session_start();
-$bdd = new PDO('mysql:host=https://isp.seblemoine.fr;dbname=bdd_chargpt;charset=utf8', 'bdd_chatgpt', 'ySdf94kAM@');
+$bdd = new PDO('mysql:host=isp.seblemoine.fr;dbname=bdd_chargpt', 'bdd_chatgpt', 'ySdf94kAM@');
 
 
 $requete = $bdd->prepare('SELECT * FROM Lieu');
@@ -24,17 +24,13 @@ $requete->execute();
 $liste = $requete->fetchAll();
 $requete->closeCursor();
 
-var_dump($liste);
-
 ?>
 <table id= "example" border="1">
 
     <thead>
     <tr>
-        <td>Titre</td>
-        <td>Année</td>
+        <td>Coordonée</td>
         <td>Résumé</td>
-        <td>Auteur</td>
 
     </tr>
     </thead>
@@ -44,7 +40,7 @@ var_dump($liste);
         ?>
         <tr>
             <td>
-                <?= $liste[$i]['coordonnee_x']+$liste[$i]['coordonnee_y']?>
+                <?= $liste[$i]['coordonnee_x']," / ",$liste[$i]['coordonnee_y']?>
             </td>
             <td>
                 <?= $liste[$i]['resume']?>
