9c21b23801fe9ff13e798e1848566453f2d11384
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2024-12-06 04:49:09 +0100

    refactore: ajour du qr code dans le tableau

--- gestion/gestionModification.php
+++ gestion/gestionModification.php
@@ -1,13 +1,14 @@
 <?php
 $bdd = new PDO('mysql:host=isp.seblemoine.fr;dbname=bdd_chargpt', 'bdd_chatgpt', 'ySdf94kAM@');
 if(isset($_POST['modifier'])) {
-    $requete = $bdd->prepare('UPDATE Lieu SET coordonnee_x =:coordonnee_x, coordonnee_y = :coordonnee_y, titre = :titre, resume = :resume WHERE id_lieu =:id');
+    $requete = $bdd->prepare('UPDATE Lieu SET coordonnee_x =:coordonnee_x, coordonnee_y = :coordonnee_y, titre = :titre, resume = :resume, qr_code = :qr_code WHERE id_lieu =:id');
     $requete->execute(array(
         'id' => $_POST['id_lieu'],
         'titre' => $_POST['titre'],
         'coordonnee_x' => $_POST['coordonnee_x'],
         'coordonnee_y' => $_POST['coordonnee_y'],
-        'resume' => $_POST['resume']
+        'resume' => $_POST['resume'],
+        'qr_code' => $_POST['qr_code']
     ));
 }
 header("location:../front/InterfaceAdmin");
